d819282723be90dbcb7b72a98390a7f174b525fd
reiitz <reiitz@users.noreply.github.com>
2025-11-06 11:01:42 +0000

    Add functional programming demos: comprehensions and lambdas

--- functional_demo.py
+++ functional_demo.py
@@ -15,3 +15,23 @@ print(list(range(21)))   # See if 20 appears
 
 evens = [num for num in range(21) if num % 2 == 0]
 print(evens)
+# Regular function
+def square(x):
+    return x ** 2
+
+# Lambda (anonymous function)
+square_lambda = lambda x: x ** 2
+
+print(square(5))          # 25
+print(square_lambda(5))   # 25
+
+# Practical use: sorting students by average
+students = [
+    {"name": "Chiamaka", "average": 87},
+    {"name": "Tunde", "average": 90},
+    {"name": "Amara", "average": 79}
+]
+
+# Sort by average using lambda
+sorted_students = sorted(students, key=lambda s: s["average"])
+print([s["name"] for s in sorted_students])  # ['Amara', 'Chiamaka', 'Tunde']
